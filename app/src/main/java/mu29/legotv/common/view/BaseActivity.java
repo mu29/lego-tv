f595f13267fa88811e2623d8d9f6dcebe007657c
InJung Chung <mu29@yeoubi.net>
2017-01-10 14:10:17 +0900

    `BaseActivity#showSnackBar` 함수를 추가한다

--- app/src/main/java/mu29/legotv/common/view/BaseActivity.java
+++ app/src/main/java/mu29/legotv/common/view/BaseActivity.java
@@ -1,7 +1,9 @@
 package mu29.legotv.common.view;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import mu29.legotv.R;
 import mu29.legotv.common.App;
@@ -50,6 +52,17 @@ public abstract class BaseActivity extends AppCompatActivity {
         switch (action.getType()) {
 
         }
+    }
+
+    protected void showSnackBar(String message) {
+        View view = findViewById(R.id.wrapper);
+        if (view == null) return;
+        showSnackBar(view, message);
+    }
+
+    protected void showSnackBar(View view, String message) {
+        Snackbar bar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        bar.show();
     }
 
     protected Dispatcher getDispatcher() {
